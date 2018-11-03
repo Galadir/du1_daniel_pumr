f44b53c9d55d188fced4d12408d1216b4c7cf0f3
Galadir <danpumr@gmail.com>
2018-11-03 15:51:54 +0100

    Přidání výpočtu poledníků a ošetření vyjímek
    Byl přidán for cyklus pro výpočet poledníků – pro všechny poledníky
    je vzorec stejný
    Dále byly řešeny vyjímky ve vstupu.
    Při chybném zadání je vždy program ukončen.
    Pro výběr zobrazeni je tak učiněno pomocí not in.
    Pro měřítko je udělena vyjímka, která vytiskne informaci,
    prč nebyl průběh úspěšný.

--- du1.py
+++ du1.py
@@ -1,37 +1,50 @@
 import math
 
 zobrazeni = input('V jakém zobrazení chcete mapu tvořit? ')
-meritko = float(input('Jaké byste si přáli měřítko? '))
 
-r=637111000/meritko
-rovnobezky = 'Rovnoběžky: '
-for i in range(-90,91,10):
-    uhel=i/180*math.pi
-    if zobrazeni == 'A':
-        epsilon = r*uhel
-    elif zobrazeni == 'L':
-        epsilon = r*math.sin(uhel)
-    elif zobrazeni == 'B':
-        epsilon =2*r*math.tan(uhel/2)
-    elif zobrazeni == 'M':
-        if abs(i) == 90:
-            epsilon = 101
-        elif i < 0:
-            uhelM = (90 / 180 * math.pi) - abs(uhel)
-            epsilon = -(r * math.log(math.cos(uhelM / 2) / math.sin(uhelM / 2)))
+if zobrazeni not in ['M','A','L','B']:
+    print('Zobrazení nebylo zadáno správně a program byl ukončen')
+else:
+    try:
+        meritko = float(input('Jaké byste si přáli měřítko? '))
+
+        if meritko <0:
+            print('Měřítko bylo zadáno chybně a program byl ukončen. Měřítko musí být vždy větší než nula.')
         else:
-            uhelM = (90/180*math.pi)-abs(uhel)
-            epsilon = r*math.log(math.cos(uhelM/2)/math.sin(uhelM/2))
-    else:
-        zobrazeni = input('Zobrazení nebylo zadáno správně.\n'
-                          'Pro spravný průběh je potřeba vybrat z možnosti M, L, B, A.\n'
-                          'Zkuste zadat zobrazení znovu: ')
-        print(i)
-        i -= 20
-        print(i)
-        continue
+            r=637111000/meritko
+            rovnobezky = 'Rovnoběžky: '
+            for i in range(-90,91,10):
+                uhel=i/180*math.pi
+                if zobrazeni == 'A':
+                    epsilon = r*uhel
+                elif zobrazeni == 'L':
+                    epsilon = r*math.sin(uhel)
+                elif zobrazeni == 'B':
+                    epsilon =2*r*math.tan(uhel/2)
+                elif zobrazeni == 'M':
+                    if abs(i) == 90:
+                        epsilon = 101
+                    elif i < 0:
+                        uhelM = (90 / 180 * math.pi) - abs(uhel)
+                        epsilon = -(r * math.log(math.cos(uhelM / 2) / math.sin(uhelM / 2)))
+                    else:
+                        uhelM = (90/180*math.pi)-abs(uhel)
+                        epsilon = r*math.log(math.cos(uhelM/2)/math.sin(uhelM/2))
+                if epsilon <=100 and epsilon >=-100:
+                    rovnobezky += ("{:.1f} ".format(epsilon))
+                else:
+                    rovnobezky += '- '
 
+            poledniky = 'Poledniky: '
+            for i in range(-180, 181, 10):
+                uhel = i / 180 * math.pi
+                ro = r * uhel
+                if ro <=100 and ro >=-100:
+                    poledniky += ("{:.1f} ".format(ro))
+                else:
+                    poledniky += '- '
 
-    rovnobezky += ("{:.1f}  ".format(epsilon))
-
-print(rovnobezky)
+            print(poledniky)
+            print(rovnobezky)
+    except ValueError:
+        print('Měřítko bylo zadáno chybně a program byl ukončen. Pravděpodobně se nejednalo o číslo')
